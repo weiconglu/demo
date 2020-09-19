c25f80745e4805c3291477e49745d137d315c83a
weiconglu <lu_weicong@purvar.co.jp>
2020-09-19 14:51:27 +0900

    no message

---
+++ src/main/java/org/test/mail/GetPOP3MailTest.java
@@ -0,0 +1,48 @@
+package org.test.mail;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.mail.Flags;
+import javax.mail.Folder;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+
+import com.sun.mail.pop3.POP3Folder;
+import com.sun.mail.pop3.POP3Store;
+
+public class GetPOP3MailTest {
+
+	public static void main(String[] args) throws MessagingException, IOException {
+
+		// 获得邮件配置文件mail.properties
+		Properties mailConfigProperties = MailHelper.getMailConfigProperties("mail.properties");
+
+		// 创建邮件会话
+		Session session = Session.getInstance(mailConfigProperties);
+		
+		// 获取POP3Store
+		POP3Store pop3Store = (POP3Store) session.getStore("pop3");
+
+		pop3Store.connect(mailConfigProperties.getProperty("mail.address"),
+				mailConfigProperties.getProperty("mail.password"));
+
+		POP3Folder folder = (POP3Folder) pop3Store.getFolder("INBOX");
+		folder.open(Folder.READ_WRITE);
+		
+		Message[] msgs = folder.getMessages();
+
+		if (null == msgs || msgs.length == 0) {
+			System.out.println("no mails found");
+			return;
+		}
+		for (int i=0;i<msgs.length;i++) {
+			Message msg = msgs[i];
+			Flags flags = msg.getFlags();
+			System.out.println("msg.getFlags(): "+flags);  // pop3邮件没有此属性
+		}
+
+	}
+
+}
