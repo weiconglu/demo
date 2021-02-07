6a700e08a9446158873577938d6a5f3bc912dd39
lu_weicong <lu_weicong@purvar.co.jp>
2021-02-07 22:30:31 +0900

    no message

--- DB/demo.sql
+++ DB/demo.sql
@@ -1,28 +1,41 @@
 DROP DATABASE IF EXISTS demo;
-CREATE DATABASE demo /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE demo /*!40100 DEFAULT CHARACTER SET UTF8MB4 */;
 USE demo;
 
 CREATE TABLE IF NOT EXISTS user (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  username varchar(20) NOT NULL,
-  password varchar(20) NOT NULL,
-  gender varchar(1),
-  birthday date,
-  PRIMARY KEY (id),
-  UNIQUE KEY name (username)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE IF NOT EXISTS tb_t_attendance_data (
-  id int PRIMARY KEY AUTO_INCREMENT,
-  loginName varchar(60) DEFAULT NULL COMMENT '',
-  fullName varchar(60) DEFAULT NULL COMMENT '',
-  status varchar(20) DEFAULT NULL COMMENT '',
-  startAt DATETIME DEFAULT NULL COMMENT '',
-  endAt DATETIME DEFAULT NULL COMMENT '',
-  deskName varchar(20) DEFAULT NULL COMMENT '',
-  siteName varchar(20) DEFAULT NULL COMMENT '',
-  floorName varchar(20) DEFAULT NULL COMMENT ''
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='csv data';
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    username VARCHAR(20) NOT NULL,
+    password VARCHAR(20) NOT NULL,
+    gender VARCHAR(1),
+    birthday DATE,
+    PRIMARY KEY (id),
+    UNIQUE KEY name (username)
+)  ENGINE=INNODB DEFAULT CHARSET=UTF8MB4;
 
 INSERT INTO user(username,gender,password,birthday) VALUES ('tom', 'm', 'tom','1989-02-15');
 INSERT INTO user(username,gender,password,birthday) VALUES ('jerry', 'f','jerry', '1992-10-14');
+
+CREATE TABLE IF NOT EXISTS tb_t_attendance_data (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    loginName VARCHAR(60) DEFAULT NULL COMMENT '',
+    fullName VARCHAR(60) DEFAULT NULL COMMENT '',
+    status VARCHAR(20) DEFAULT NULL COMMENT '',
+    startAt DATETIME DEFAULT NULL COMMENT '',
+    endAt DATETIME DEFAULT NULL COMMENT '',
+    deskName VARCHAR(20) DEFAULT NULL COMMENT '',
+    siteName VARCHAR(20) DEFAULT NULL COMMENT '',
+    floorName VARCHAR(20) DEFAULT NULL COMMENT ''
+)  ENGINE=INNODB DEFAULT CHARSET=UTF8MB4 COMMENT='csv data';
+
+CREATE TABLE IF NOT EXISTS tb_t_employee (
+    id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    last_name VARCHAR(50),
+    email VARCHAR(50),
+    gender CHAR(1),
+    age INT
+)  ENGINE=INNODB DEFAULT CHARSET=UTF8MB4 COMMENT='employee';
+
+INSERT INTO tb_t_employee(last_name,email,gender,age) VALUES('Tom','tom@atguigu.com',1,22);
+INSERT INTO tb_t_employee(last_name,email,gender,age) VALUES('Jerry','jerry@atguigu.com',0,25);
+INSERT INTO tb_t_employee(last_name,email,gender,age) VALUES('Black','black@atguigu.com',1,30);
+INSERT INTO tb_t_employee(last_name,email,gender,age) VALUES('White','white@atguigu.com',0,35)
